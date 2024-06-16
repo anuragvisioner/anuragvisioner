7b43e9054cff23e2054b034db159ee2b92cd7b94
ANURAG SHARMA <146327546+anuragvisioner@users.noreply.github.com>
2024-06-16 12:22:57 +0530

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src="/path/to/github-header-image.png" alt="GitHub Banner" className="banner-image" />
+    </div>
+  );
+};
+
+export default Banner;
